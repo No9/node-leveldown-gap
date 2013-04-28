0342bf747efff44c6202c9f9424ce27ebc377983
No9 <antonwhalley@vodafone.ie>
2013-04-28 14:06:35 +0100

    included all tests

--- test.js
+++ test.js
@@ -8,13 +8,12 @@ var tape   = require('tape')
   , testBuffer = new Uint8Array(1)
 
 testBuffer[0] = '☃'
-console.dir(factory);
 
-//require('abstract-leveldown/abstract/leveldown-test').args(factory, tape)
-//require('abstract-leveldown/abstract/open-test').args(factory, tape, testCommon)
-//require('abstract-leveldown/abstract/del-test').all(factory, tape, testCommon)
-//require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon)
-//require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon)
-//require('abstract-leveldown/abstract/put-get-del-test').all(factory, tape, testCommon, testBuffer)
-//require('abstract-leveldown/abstract/close-test').close(factory, tape, testCommon)
+require('abstract-leveldown/abstract/leveldown-test').args(factory, tape)
+require('abstract-leveldown/abstract/open-test').args(factory, tape, testCommon)
+require('abstract-leveldown/abstract/del-test').all(factory, tape, testCommon)
+require('abstract-leveldown/abstract/put-test').all(factory, tape, testCommon)
+require('abstract-leveldown/abstract/get-test').all(factory, tape, testCommon)
+require('abstract-leveldown/abstract/put-get-del-test').all(factory, tape, testCommon, testBuffer)
+require('abstract-leveldown/abstract/close-test').close(factory, tape, testCommon)
 require('abstract-leveldown/abstract/iterator-test').all(factory, tape, testCommon)
